3012173b6354ea5f246aac3dfac355b8813ff572
goldenboy070S <165132734+goldenboy070S@users.noreply.github.com>
2024-10-21 16:53:12 +0500

    Delete Daily_cost directory

--- Daily_cost/main/serializers.py
+++
@@ -1,9 +0,0 @@
-from .models import Daily_cost
-from rest_framework.serializers import ModelSerializer
-
-
-class Daily_costSerializer(ModelSerializer):
-    class Meta:
-        model = Daily_cost
-        fields = '__all__'
-        
--- Daily_cost/main/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import Daily_costViewSet
-
-router = DefaultRouter()
-
-router.register('daily_costs', Daily_costViewSet)
-
-urlpatterns = router.urls
