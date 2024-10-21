b0bf6ef0168c03fb216558e86d54c424150001db
goldenboy070S <165132734+goldenboy070S@users.noreply.github.com>
2024-10-21 16:54:22 +0500

    Delete retsept directory

--- retsept/main/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import path
-# from .views import *
-
-# urlpatterns = [
-#     path('', ReteptViewSet.as_view({'get': 'list', 'post': 'create'})),
-#     path('<int:pk>', ReteptViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-# ]
